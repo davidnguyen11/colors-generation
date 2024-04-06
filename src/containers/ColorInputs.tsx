b7c0e9a8042d08c65358663269cf3b7fb3999de3
Dzung Nguyen <dzungnguyen@Dzungs-MacBook-Air.local>
2024-04-06 18:48:29 +0900

    Improve UI

---
+++ src/containers/ColorInputs.tsx
@@ -0,0 +1,65 @@
+import { transparentize } from "polished";
+
+import { InputSection } from "@/components/input-section";
+import { useState } from "react";
+import { hexify } from "@/utils/hexify";
+import { isValidHexColor } from "@/utils/isValidHexColor";
+
+export function ColorInputs() {
+  const [hexColor, setHexColor] = useState("");
+  const [percentages, setPercentages] = useState<number[]>([]);
+
+  const onGenerateColors = (values: number[]) => {
+    setPercentages(values);
+  };
+
+  const onHexColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHexColor(e.target.value);
+  };
+
+  return (
+    <div >
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-3">
+          Source color
+          <input
+            placeholder="Source color"
+            className="input input-md input-bordered input-accent"
+            onChange={onHexColorChange}
+          />
+        </div>
+
+        <InputSection onGenerateColors={onGenerateColors} />
+      </div>
+
+      <div className="flex flex-col gap-5 mt-5">
+        {isValidHexColor(hexColor) && (
+          <div
+            className="w-36 h-36 p-2 rounded-lg"
+            style={{ backgroundColor: hexColor }}
+          >
+            Source color - {hexColor}
+          </div>
+        )}
+        <div className="flex flex-wrap gap-5">
+          {percentages.map((percentage, index) => {
+            const percentageValue = isNaN(percentage) ? 0 : percentage;
+
+            const per = percentageValue / 100;
+            const bgColor = hexify(transparentize(1 - per, hexColor));
+
+            return (
+              <div
+                key={`polished-${index}`}
+                className="w-36 h-36 p-2 rounded-lg"
+                style={{ backgroundColor: bgColor }}
+              >
+                {percentageValue}% - {bgColor}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,29 @@
-import { transparentize } from "polished";
-
-import { InputSection } from "@/components/input-section";
 import { useState } from "react";
-import { hexify } from "@/utils/hexify";
-import { isValidHexColor } from "@/utils/isValidHexColor";
+import { ColorInputs } from '@/containers/ColorInputs'
+import { Plus } from "@/components/icons";
 
 export default function Home() {
-  const [hexColor, setHexColor] = useState("");
-  const [percentages, setPercentages] = useState<number[]>([]);
+  const [numberColorInputs, setNumberColorInputs] = useState(1);
+  const colorInputs = Array.from({ length: numberColorInputs });
 
-  const onGenerateColors = (values: number[]) => {
-    setPercentages(values);
-  };
+  const handleAddColor = () => {
+    setNumberColorInputs(numberColorInputs + 1)
+  }
 
-  const onHexColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setHexColor(e.target.value);
-  };
 
   return (
     <main className="container mx-auto w-3/6">
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col gap-3">
-          Source color
-          <input
-            placeholder="Source color"
-            className="input input-md input-bordered input-accent"
-            onChange={onHexColorChange}
-          />
+      {colorInputs.map((_, index) =>
+        <div key={index}>
+          <ColorInputs />
+          <div className="divider" />
         </div>
+      )}
 
-        <InputSection onGenerateColors={onGenerateColors} />
-      </div>
-
-      <div className="flex flex-col gap-5 mt-5">
-        {isValidHexColor(hexColor) && (
-          <div
-            className="w-36 h-36 p-2 rounded-lg"
-            style={{ backgroundColor: hexColor }}
-          >
-            Source color - {hexColor}
-          </div>
-        )}
-        <div className="flex flex-wrap gap-5">
-          {percentages.map((percentage, index) => {
-            const percentageValue = isNaN(percentage) ? 0 : percentage;
-
-            const per = percentageValue / 100;
-            const bgColor = hexify(transparentize(1 - per, hexColor));
-
-            return (
-              <div
-                key={`polished-${index}`}
-                className="w-36 h-36 p-2 rounded-lg"
-                style={{ backgroundColor: bgColor }}
-              >
-                {percentageValue}% - {bgColor}
-              </div>
-            );
-          })}
-        </div>
+      <div className="fixed bottom-5 right-5">
+        <button className="btn btn-neutral btn-circle" onClick={handleAddColor}>
+          <Plus />
+        </button>
       </div>
     </main>
   );
